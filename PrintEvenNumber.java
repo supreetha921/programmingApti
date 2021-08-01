3024f171dc3b6f2240aec7a904f09dfcd5b13e26
supreetha921 <gcsupritha22@gmail.com>
2021-08-01 09:32:45 +0530

    program to print number based on given condition

---
+++ PrintEvenNumber.java
@@ -0,0 +1,28 @@
+class PrintEvenNumber{
+ public static void main(String[]a){
+	 
+	
+ 
+ int i=1;
+ print(i);
+ }
+ 
+ 
+ static void print(int i){
+ if(i<=50 ){
+	 if(i%2==0)
+ System.out.println(i);
+ i++;
+ print(i);
+ 
+ }
+ }
+ 
+ 
+ 
+ 
+ 
+ }
+ 
+ 
+ 
